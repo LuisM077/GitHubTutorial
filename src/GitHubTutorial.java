ced5104f934cb5f83e48ee3ed92edd596546c4cd
josemorales <josemorales@134.210.229.101>
2024-09-16 19:41:22 -0400

    addition

--- src/GitHubTutorial.java
+++ src/GitHubTutorial.java
@@ -11,8 +11,15 @@ public class GitHubTutorial
         Random random = new Random();
         String str = "Hello World!";
         int age = random.nextInt(100);
+        int a;
+        int b;
         
         System.out.println(str + " Today I turned " +  age + " :p");
+        
+        a = random.nextInt(10 + 1);
+        b = random.nextInt(10+1);
+        System.out.println("a = " + a + " b = " + b);
+        System.out.println("a + b = " + a + b);
     }
     
 }
